cd23305405d2f0a2058612d4bcd0a18d6b7de337
Jefwillems <jefwillems100@gmail.com>
2018-11-14 11:33:42 +0100

    add zoom-out function when clicked on empty canvas

--- src/components/Graph.vue
+++ src/components/Graph.vue
@@ -18,121 +18,129 @@
 </template>
  
 <script>
-  import { mapState, mapGetters } from 'vuex';
-  import types from '../store/types';
-  import { legend } from '../store';
-  // import CitySelection from './CitySelection';
-  import { Network } from 'vis';
+import { mapState, mapGetters } from 'vuex';
+import types from '../store/types';
+import { legend } from '../store';
+// import CitySelection from './CitySelection';
+import { Network } from 'vis';
 
-  export default {
-    name: 'Graph',
-    // components: { CitySelection },
-    data() {
-      return {
-        events: ['doubleClick', 'selectNode'],
-        network: null,
-        dialog: false
-      };
-    },
-    props: {
-      width: {
-        type: String,
-        default: function() {
-          return '100%';
-        }
-      },
-      height: {
-        type: String,
-        default: function() {
-          return '100%';
-        }
+export default {
+  name: 'Graph',
+  // components: { CitySelection },
+  data() {
+    return {
+      events: ['doubleClick', 'selectNode', 'click'],
+      network: null,
+      dialog: false
+    };
+  },
+  props: {
+    width: {
+      type: String,
+      default: function() {
+        return '100%';
       }
     },
-    computed: {
-      ...mapState([]),
-      ...mapGetters(['get_nodes_and_edges', 'get_options', 'btnText', 'topic'])
-    },
-    created() {},
-    mounted() {
+    height: {
+      type: String,
+      default: function() {
+        return '100%';
+      }
+    }
+  },
+  computed: {
+    ...mapState([]),
+    ...mapGetters(['get_nodes_and_edges', 'get_options', 'btnText', 'topic'])
+  },
+  created() {},
+  mounted() {
+    this.dialog = true;
+    this.$store.dispatch(types.FETCH_SPREADSHEET_DATA);
+  },
+  methods: {
+    reload() {
       this.dialog = true;
+      this.$store.state.topic = '';
       this.$store.dispatch(types.FETCH_SPREADSHEET_DATA);
     },
-    methods: {
-      reload() {
-        this.dialog = true;
-        this.$store.state.topic = '';
-        this.$store.dispatch(types.FETCH_SPREADSHEET_DATA);
-      },
-      handle_event(ev, props) {
-        switch (ev) {
-          case 'doubleClick':
-            this.dialog = true;
-            this.$store.commit(types.HANDLE_CLICK, props);
-            break;
-          case 'selectNode':
-            //? current scale = Math.round(this.network.getScale()*10)/10
-            this.network.focus(props.nodes[0], {
+    handle_event(ev, props) {
+      switch (ev) {
+        case 'doubleClick':
+          this.dialog = true;
+          this.$store.commit(types.HANDLE_CLICK, props);
+          break;
+        case 'selectNode':
+          //? current scale = Math.round(this.network.getScale()*10)/10
+          this.network.focus(props.nodes[0], {
+            animation: { duration: 800, easingFunction: 'easeInCubic' },
+            scale: 1.3
+          });
+          break;
+        case 'click':
+          if (props.nodes.length === 0 && props.edges.length === 0) {
+            this.network.fit({
               animation: { duration: 800, easingFunction: 'easeInCubic' },
               scale: 1.3
             });
-            break;
-          default:
-            return;
-        }
-      }
-    },
-    watch: {
-      get_nodes_and_edges: function(newGraph) {
-        if (this.network !== null) {
-          this.network.destroy();
-        }
-        const container = this.$refs.graph;
-        const x = -container.clientWidth / 2 - 50;
-        let y = -container.clientHeight / 2 - 50;
-        const legend_positioned = legend.map(n => {
-          const { font: f } = n;
-          const node = { ...n, x, y, font: { ...f, vadjust: -15 } };
-          y += 80;
-          return node;
-        });
-
-        newGraph.nodes.add
-          ? newGraph.nodes.add(legend_positioned)
-          : newGraph.nodes.push(...legend_positioned);
-
-        this.network = new Network(container, newGraph, this.get_options);
-        this.events.forEach(ev =>
-          this.network.on(ev, props => this.handle_event(ev, props))
-        );
-
-        this.network.on('afterDrawing', () => (this.dialog = false));
-        this.network.on('stabilizationIterationsDone', () => {
-          this.network.setOptions({ physics: false });
-        });
+          }
+          break;
+        default:
+          return;
       }
     }
-  };
+  },
+  watch: {
+    get_nodes_and_edges: function(newGraph) {
+      if (this.network !== null) {
+        this.network.destroy();
+      }
+      const container = this.$refs.graph;
+      const x = -container.clientWidth / 2 - 50;
+      let y = -container.clientHeight / 2 - 50;
+      const legend_positioned = legend.map(n => {
+        const { font: f } = n;
+        const node = { ...n, x, y, font: { ...f, vadjust: -15 } };
+        y += 80;
+        return node;
+      });
+
+      newGraph.nodes.add
+        ? newGraph.nodes.add(legend_positioned)
+        : newGraph.nodes.push(...legend_positioned);
+
+      this.network = new Network(container, newGraph, this.get_options);
+      this.events.forEach(ev =>
+        this.network.on(ev, props => this.handle_event(ev, props))
+      );
+
+      this.network.on('afterDrawing', () => (this.dialog = false));
+      this.network.on('stabilizationIterationsDone', () => {
+        this.network.setOptions({ physics: false });
+      });
+    }
+  }
+};
 </script> 
 
 <style>
-  .explorerContainer {
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-    height: 100vh;
-    max-height: 100vh;
-    width: 100vw;
-  }
-  #graph {
-    background-color: #fefefe;
-    width: 100%;
-    height: 90%;
-  }
-  #topic {
-    height: 5%;
-  }
-  #reloadBtn {
-    height: 5%;
-  }
+.explorerContainer {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+  max-height: 100vh;
+  width: 100vw;
+}
+#graph {
+  background-color: #fefefe;
+  width: 100%;
+  height: 90%;
+}
+#topic {
+  height: 5%;
+}
+#reloadBtn {
+  height: 5%;
+}
 </style>
